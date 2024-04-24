cf91a715aa5fca764409d81494d3e338dcbbcdfc
kasumi.kamizato <kasumi.kamizato@AdminnoMacBook-Pro.local>
2024-04-24 17:46:58 +0900

    laravelレビュー修正しました

--- app/database/factories/user_walking.php
+++ app/database/factories/user_walking.php
@@ -2,12 +2,15 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Userworking;
+use App\UserWalking;
+use App\Models\User;
 use Faker\Generator as Faker;
 
-$factory->define(App\Userworking::class, function (Faker $faker) {
+$factory->define(App\UserWalking::class, function (Faker $faker) {
+    $user = User::inRandomOrder()->first();
+
     return [
-        'user_id' => $faker->randomNumber(),
+        'user_id' => $user->id,
         'walking_on' => $faker->date(),
         'step_cnt' => $faker->randomNumber(),
     ];
--- app/database/seeds/Users_walkingTableSeeder.php
+++ app/database/seeds/Users_walkingTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Userworking;
+use App\UserWalking;
 
 class Users_walkingTableSeeder extends Seeder
 {
@@ -12,6 +12,6 @@ class Users_walkingTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Userworking::class, 10)->create();
+        factory(App\UserWalking::class, 5)->create();
     }
 }
